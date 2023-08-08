80eb10d1fbc26cc50aae16c121ddf4a2673e88fc
Miha <miromanovsky@gmail.com>
2023-08-08 17:04:06 +0800

    Initial Commit

---
+++ newsExplorerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newsExplorerApp
+//
+//  Created by Mikhayl Romanovsky on 2023/8/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
